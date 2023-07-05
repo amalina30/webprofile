b4649ca982b2f61c6e8305e8d56e958b43e6b857
amalina30 <amalina3037@gmail.com>
2023-07-05 15:08:55 +0700

    Pertemuan 12 yoi

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,7 +32,7 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                <NavLink :href="route('posts.index')" :active="route().current('posts')">
                                     Post
                                 </NavLink>
                             </div>
